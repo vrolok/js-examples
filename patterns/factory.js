736ffdf5f46688e381b9e07503dd0c42e81c12dc
Vlad Bereznyuk <vbereznuk@gmail.com>
2019-06-27 12:50:40 +0500

    abstract factory helps to register & use myriad of Factories all at once

--- patterns/factory.js
+++ patterns/factory.js
@@ -40,3 +40,43 @@ var v1 = factory.createServer({
 });
 
 console.log(v1); // VirtualServer { cores: 1, ram: 1024, disk: '50GB' }
+
+// Abstract Factory
+
+var abstractServerFactory = (function abstractFactory(){
+  var types = {};
+
+  return {
+    getFactory(type, obj) {
+      var Factory = types[type];
+
+      return Factory ? new Factory(obj) : undefined;
+    },
+    registerFactory(type, Factory) {
+      types[type] = Factory;
+
+      return abstractServerFactory;
+    }
+  }
+}());
+
+function StorageServer(options) {
+  this.disk = options.disk || '1TB';
+  this.raid = options.raid || 5;
+}
+
+abstractServerFactory.registerFactory('storage', StorageServer);
+abstractServerFactory.registerFactory('physical', PhysicalServer);
+
+var bucket = abstractServerFactory.getFactory('storage', {
+  disk: '2TB',
+  raid: 6
+})
+
+var p1 = abstractServerFactory.getFactory('physical', {
+  cpu:'Intel Xeon',
+  ram: '64GB'
+})
+
+console.log(bucket);
+console.log(p1);
