3f71c13218bc1e2cd8238a48ee070ff91dc3e9f0
Vlad Bereznyuk <vbereznuk@gmail.com>
2019-06-27 11:20:37 +0500

    factory pattern based on creating virtual/physical server objects

---
+++ patterns/factory.js
@@ -0,0 +1,42 @@
+// Factory Pattern
+
+function VirtualServer(options) {
+  this.cores = options.cpu || 1;
+  this.ram = options.ram || 512;
+  this.disk = options.disk || '25GB';
+}
+
+function PhysicalServer(options) {
+  this.cpu = options.cpu || 'AMD Opteron';
+  this.ram = options.ram || '32GB';
+  this.disk = options.disk || '250GB';
+  this.raid = options.raid || 'none';
+}
+
+function ServerFactory() {}
+
+ServerFactory.prototype.serverClass = VirtualServer;
+
+// the meat
+ServerFactory.prototype.createServer = function serverFactory(options) {
+  switch(options.serverType) {
+    case 'virtual':
+      this.serverClass = VirtualServer;
+      break;
+    case 'physical':
+      this.serverClass = PhysicalServer;
+      break;
+  }
+  
+  return new this.serverClass(options);
+}
+
+var factory = new ServerFactory();
+
+var v1 = factory.createServer({
+  serverType: 'virtual',
+  disk: '50GB',
+  ram: 1024
+});
+
+console.log(v1); // VirtualServer { cores: 1, ram: 1024, disk: '50GB' }
