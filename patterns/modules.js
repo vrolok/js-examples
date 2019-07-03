8c045a849eeb32bae7f72b610d9f504925bba52d
Vlad Bereznyuk <vbereznuk@gmail.com>
2019-07-03 18:17:39 +0500

    added module pattern variation

--- patterns/modules.js
+++ patterns/modules.js
@@ -1,6 +1,27 @@
+// Module Pattern
+
+;(function iefeModule(globalThis) {
+  // globalThis - global var that guesses wich global object is in use;
+  function MyModule() {
+    this.myMethod = function myMethod() {
+      console.log('myMethod');
+    };
+    this.myOtherMethod = function myOtherMethod() {
+      console.log('myOtherMethod');
+    };
+  }
+
+  // expose constructor
+  globalThis.MyModule = MyModule;
+}(globalThis));
+
+var module1 = new MyModule(); // global execution context will find it :)
+module1.myMethod();
+module1.myOtherMethod();
+
 // Revealing Module Pattern
 
-var module = (function iefe() {
+var module2 = (function iefeRevModule() {
   var privateVar = 'private';
 
   var privateFunc = () => { console.log('function'); };
@@ -12,6 +33,6 @@ var module = (function iefe() {
   };
 }());
 
-module.setVar('not private');
-console.log(module.getVar());
-module.runme();
+module2.setVar('not private');
+console.log(module2.getVar());
+module2.runme();
